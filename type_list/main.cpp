dd46dd5a67262f38f356f25a324bdf8b7a584dfc
EgorovMike219 <mike219@mail.ru>
2018-10-24 14:31:28 +0300

    не работает Length(проблема в форматах)

--- type_list/main.cpp
+++ type_list/main.cpp
@@ -3,59 +3,61 @@
 #include "typeList.h"
 
 
-using CharList = TypeList<char, signed char, unsigned char>;
-using List = NTypeList::Erase<TypeList<char, signed char, unsigned char>, double>::result;
-using SignedCharList = NTypeList::Erase<CharList, unsigned char>::result;
+using CharIntList = TypeList<char, signed char, unsigned char, int, signed int, unsigned int>;
+using List = Erase<CharIntList, double>::result;
+using SignedCharList = Erase<CharIntList, unsigned char>::result;
+using EmpltyList = TypeList<>;
 
 
-template <class T>
-struct FImpl
-{
-    static void f() // users, go ahead and specialize this
-    {
-        std::cout<<typeid(T).name()<<std::endl;
-    }
-};
+template<class T>
+struct FImpl;
 
 template<class T>
 void printTypeList() { FImpl<T>::f(); } // users, don't touch this!
 
-template <>
-struct FImpl<TypeList<NullType> >
-{
-    static void f() // users, go ahead and specialize this
-    {}
-};
+using EmptyList = TypeList<>;
 
-template <class Head, class ... Tail>
-struct FImpl<TypeList<Head, TypeList<Tail ...> > >
-{
+
+
+int main() {
+    std::cout << "Char Int List!" << std::endl;
+    printTypeList<CharIntList>();
+    std::cout<<"Length="<<Length<CharIntList>::value<<std::endl;
+    std::cout << "List!" << std::endl;
+    printTypeList<List>();
+    std::cout<<"Length="<<Length<List>::value<<std::endl;
+    std::cout << "Signed Char List!" << std::endl;
+    printTypeList<SignedCharList>();
+    std::cout<<"Length="<<Length<SignedCharList>::value<<std::endl;
+    std::cout<<"Length empty list="<<Length<EmptyList>::value<<std::endl;
+    return 0;
+}
+
+
+template<>
+struct FImpl<TypeList<NullType> > {
     static void f() // users, go ahead and specialize this
     {
-        std::cout<<typeid(Head).name()<<std::endl;
+//        std::cout<<"TypeList<NullType>"<<std::endl;
+    }
+};
+
+template<class Head, class ... Tail>
+struct FImpl<TypeList<Head, TypeList<Tail ...> > > {
+    static void f() // users, go ahead and specialize this
+    {
+//        std::cout<<"TypeList<Head, TypeList<Tail ...>"<<std::endl;
+        std::cout << typeid(Head).name() << std::endl;
         printTypeList<TypeList<Tail ...> >();
     }
 };
 
 //// для поддержки c++11
-template <class Head, class ... Tail>
-struct FImpl<TypeList<Head, Tail ...> >
-{
+template<class Head, class ... Tail>
+struct FImpl<TypeList<Head, Tail ...> > {
     static void f() // users, go ahead and specialize this
     {
-        printTypeList<TypeList<Head, class TypeList<Head, Tail ...>::tail > >();
+//        std::cout<<"TypeList<Head, Tail ...>"<<std::endl;
+        printTypeList<TypeList<Head, class TypeList<Head, Tail ...>::tail> >();
     }
 };
-
-
-int main() {
-    std::cout << "Char List!" << std::endl;
-    printTypeList<CharList>();
-    std::cout << "List!" << std::endl;
-    printTypeList<List>();
-    std::cout << "<char, signed char, unsigned char>!" << std::endl;
-    printTypeList<TypeList<char, signed char, unsigned char>>();
-    std::cout << "Signed Char List!" << std::endl;
-    printTypeList<SignedCharList>();
-    return 0;
-}
--- type_list/typeList.h
+++ type_list/typeList.h
@@ -1,8 +1,9 @@
 #pragma once
 
+
 class NullType {};
 
-template <class T, class ... U>
+template <class T=NullType, class ... U>
 struct TypeList
 {
     using head = T;
@@ -16,40 +17,69 @@ struct TypeList<T>
     using tail = NullType;
 };
 
-
-
-
-namespace NTypeList
+// удаление только первого вхождения
+template <class Type, class T> struct Erase
 {
-    // удаление только первого вхождения
-    template <class Type, class T> struct Erase
-    {
-        using result = NullType;  // этот код никогда не выполнится, нужен для видимости result
+    using result = NullType;  // этот код никогда не выполнится, нужен для видимости result
+};
+// если typeList пустой, то результат NullType
+template <class T>
+struct Erase<TypeList<NullType>, T>
+{
+    using result = NullType;
+};
+// иначе
+// если T=Head, то результат хвост
+template <class T, class ... Tail>
+struct Erase<TypeList<T, TypeList<Tail ...> >, T>
+{
+    using result = TypeList<Tail ...>;
+};
+// иначе
+// рекурсивно удаляем элемент из хвоста
+template <class Head, class ... Tail, class T>
+struct Erase<TypeList<Head, TypeList<Tail ...> >, T>
+{
+    using result = TypeList<Head, class Erase<class TypeList<Head, Tail ...>::tail, T>::result >;
+};
+// для поддержки с++ templates
+template <class Head, class ... Tail, class T>
+struct Erase<TypeList<Head, Tail ...>, T>
+{
+    using result = class Erase<TypeList<Head, class TypeList<Head, Tail ...>::tail >, T>::result;
+};
+
+#include <iostream>
+
+template <class TypeList>
+struct Length {
+    enum {
+        value = Length<class TypeList::tail>::value + 1
     };
-    // если typeList пустой, то результат NullType
-    template <class T>
-    struct Erase<TypeList<NullType>, T>
-    {
-        using result = NullType;
+    static int f() {
+        std::cout<<"TypeList| TypeList::tail="<< typeid(class TypeList::tail).name()<<std::endl;
+        return 1 + Length<class TypeList::tail>::f();
+    }
+};
+
+template <>
+struct Length<TypeList<NullType> > {
+    enum {
+        value = 0
     };
-    // иначе
-    // если T=Head, то результат хвост
-    template <class T, class ... Tail>
-    struct Erase<TypeList<T, TypeList<Tail ...> >, T>
-    {
-        using result = TypeList<Tail ...>;
+    static int f() {
+        std::cout<<"TypeList<NullType>"<<std::endl;
+        return 0;
+    }
+};
+
+template <>
+struct Length<NullType> {
+    enum {
+        value = 0
     };
-    // иначе
-    // рекурсивно удаляем элемент из хвоста
-    template <class Head, class ... Tail, class T>
-    struct Erase<TypeList<Head, TypeList<Tail ...> >, T>
-    {
-        using result = TypeList<Head, class Erase<class TypeList<Head, Tail ...>::tail, T>::result >;
-    };
-    // для поддержки с++ templates
-    template <class Head, class ... Tail, class T>
-    struct Erase<TypeList<Head, Tail ...>, T>
-    {
-        using result = class Erase<TypeList<Head, class TypeList<Head, Tail ...>::tail >, T>::result;
-    };
-}
+    static int f() {
+        std::cout<<"NullType"<<std::endl;
+        return 0;
+    }
+};
